315cd0127632c0c8e02fd75dc8c1fa18bfa1606e
Evix <giraudeau.valentin@gmail.com>
2022-09-21 16:20:47 +0200

    Added a program to calculate the sum of geometric progression and correct the arithmetic progression sum program

--- SommeSuiteGeometrique.py
+++ SommeSuiteGeometrique.py
@@ -1,14 +1,14 @@
 import math
 
-def SommeArithmetique():
-	u0=input("Est-ce que vous cherchez la somme des n+1 premiers termes (U0)? Répondre par oui ou par non ")
+def SommeSuiteGeometrique():
+	u0=input("Est-ce que vous cherchez la somme des n+1 premiers termes (U0) ? Répondre par oui ou par non ")
 	u1=input("Est-ce que vous cherchez la somme des n premiers termes (U1)? Répondre par oui ou par non ")
 
 	if u0==("oui") :
 		u=float(input("U0 = ? "))
-		n=int(input("nombre de termes n : "))
-		un=float(input("Un = ?"))
-		s=(n+1)*(u+n)/2
+		n=int(input("nombre de termes n ? "))
+		q=float(input("raison q ? "))
+		s=u*((1-q**(n+1))/(1-q))
 		print("S= ",s)
 	elif u0==("non"):
 		print("")
@@ -17,21 +17,13 @@ def SommeArithmetique():
 
 	if u1==("oui") :
 		u=float(input("U1 = ? "))
-		n=int(input("nombre de termes n : "))
-		un=float(input("Un = ?"))
-		s=n*((u+n)/2)
+		n=int(input("nombre de termes n ? "))
+		q=float(input("raison q ? "))
+		s=u*((1-q**n)/(1-q))
 		print("S= ",s)
 	elif u1==("non"):
 		print("")
 	else:
 		print("Erreur chercher U0 ou U1 avant de lancer le programme")
 
-
-
-SommeArithmetique()
-
-
-
-#Formules
-#(n+1)*(u0+un)/2
-#n*((u0+un)/2)
+SommeSuiteGeometrique()
